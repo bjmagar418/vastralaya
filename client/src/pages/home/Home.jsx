cada1f283075b481f87d253c199977555329cea4
bjmagar418 <bjmagar418@gmail.com>
2026-05-14 22:06:34 +0545

    added homepage

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from './Banner'
+import Herosection from './Herosection'
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Herosection/>
+    </>
+  )
+}
+
+export default Home
